5990c55c10348cf0bfc3bf38089932040e8ce15f
fernando <fernando.babonoyaba@gmail.com>
2022-02-02 08:20:36 -0700

    Initial Commit

---
+++ HIITFit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HIITFit
+//
+//  Created by fernando babonoyaba on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
